69ac5264a470e1e7dbe2fd48e50e3aa3f2009e94
bruned <2136407907@qq.com>
2021-03-01 15:08:17 +0800

    更新事件系统

--- src/main/java/top/bruned/kaiheila/framework/event/EventChannel.java
+++ src/main/java/top/bruned/kaiheila/framework/event/EventChannel.java
@@ -2,21 +2,46 @@ package top.bruned.kaiheila.framework.event;
 
 import top.bruned.kaiheila.framework.plugin.annotation.EventHandler;
 import top.bruned.kaiheila.sdk.util.Log;
-import top.bruned.kaiheila.sdk.wsclient.result.event.GroupTextMessageEvent.GroupTextMessageEvent;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+
+import top.bruned.kaiheila.sdk.wsclient.result.event.GroupAddedReactionEvent.GroupAddedReactionEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.GroupDeletedMessageEvent.GroupDeletedMessageEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.GroupDeletedReactionEvent.GroupDeletedReactionEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.GroupExitedChannelEvent.GroupExitedChannelEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.GroupImageMessageEvent.GroupImageMessageEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.GroupJoinedChannelEvent.GroupJoinedChannelEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.GroupTextMessageEvent.GroupTextMessageEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.PersonAddedReactionEvent.PersonAddedReactionEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.PersonDeletedMessageEvent.PersonDeletedMessageEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.PersonDeletedReactionEvent.PersonDeletedReactionEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.PersonGuildMemberOfflineEvent.PersonGuildMemberOfflineEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.PersonGuildMemberOnlineEvent.PersonGuildMemberOnlineEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.PersonImgaeMessageEvent.PersonImageMessageEvent;
+import top.bruned.kaiheila.sdk.wsclient.result.event.PersonTextMessageEvent.PersonTextMessageEvent;
 
 public class EventChannel {
-    private HashMap<Object,Object> eventRunList= new HashMap<>();
-    private final List<PluginMethod> GroupTextMessageEventList = new ArrayList<>();
-    private final List<PluginMethod> PluginMethodList = new ArrayList<>();
+    private HashMap<Class,List<PluginMethod>> eventRunList= new HashMap<Class,List<PluginMethod>>();
+    private final List<PluginMethod> PluginMethodList = new ArrayList<PluginMethod>();
     private final Log log = new Log("事件管道");
 
+    public EventChannel(){
+        List<Class> registerEvent = Arrays.asList(
+                GroupAddedReactionEvent.class, GroupDeletedReactionEvent.class,
+                GroupDeletedMessageEvent.class, GroupExitedChannelEvent.class,
+                GroupImageMessageEvent.class, GroupJoinedChannelEvent.class,
+                GroupTextMessageEvent.class, PersonAddedReactionEvent.class,
+                PersonDeletedMessageEvent.class, PersonDeletedReactionEvent.class,
+                PersonGuildMemberOfflineEvent.class, PersonGuildMemberOnlineEvent.class,
+                PersonImageMessageEvent.class, PersonTextMessageEvent.class
+        );
+        for (Class clz : registerEvent){
+            eventRunList.put(clz,new ArrayList<PluginMethod>());
+        }
+    }
     public void registerEventClass(Object eventObject) {
         Class clz = eventObject.getClass();
         Method[] methods = clz.getDeclaredMethods();
@@ -34,33 +59,26 @@ public class EventChannel {
     }
 
     public void eventBroadCast(Object event) {
-        if (event.getClass() == GroupTextMessageEvent.class) {
-            for (PluginMethod method : GroupTextMessageEventList) {
-
-                try {
-                    method.getMethod().invoke(method.getEventObject(), event);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    log.debug(e.getCause().getMessage());
-                    e.printStackTrace();
-                }
-
+        for (PluginMethod method : eventRunList.get(event.getClass())) {
+            try {
+                method.getMethod().invoke(method.getEventObject(), event);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                log.debug(e.getCause().getMessage());
+                e.printStackTrace();
             }
         }
     }
 
     public void init() {
-        List<PluginMethod> GroupMessageEventPluginTemp = new ArrayList<>();
         for (PluginMethod pluginMethod : PluginMethodList) {
-            Class event = pluginMethod.getEventtype();
-
-            if (GroupTextMessageEvent.class.equals(event)) {
-                GroupMessageEventPluginTemp.add(pluginMethod);
-            }
+            eventRunList.get(pluginMethod.getEventtype()).add(pluginMethod);
         }
-        GroupMessageEventPluginTemp.sort(new PluginsComparator());
-        GroupTextMessageEventList.addAll(GroupMessageEventPluginTemp);
+        for (Map.Entry<Class,List<PluginMethod>> map : eventRunList.entrySet()){
+            eventRunList.get(map.getKey()).sort(new PluginsComparator());
+        }
+        log.debug(eventRunList.toString());
         log.debug("注册完毕");
     }
 
